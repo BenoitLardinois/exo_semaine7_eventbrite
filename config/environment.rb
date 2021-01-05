7c79c03864aff4aae223f5fbe9aa5300972e02e3
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-05 16:19:46 +0300

    pushing to Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   port: 587,
   address: 'smtp.gmail.com',
   user_name: ENV['SMTP_USER_NAME'],
-  password: ENV['SMTP_USER_PASSWORD'],
+  password: ENV['SMTP_PASSWORD'],
   domain: 'gmail.com',
   authentication: :plain,
   enable_starttls_auto: true
--- config/initializers/smtp.rb
+++
@@ -1,6 +0,0 @@
-=begin
-AWS::SMTP::Base.establish_connection!(
- :user_name   => ENV['SMTP_USER_NAME'],
- :password => ENV['SMTP_USER_PASSWORD']
-)
-=end
