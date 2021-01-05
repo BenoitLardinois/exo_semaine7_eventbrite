84c07878b1fa6b06abc97b64639765e88e6c196d
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-05 15:46:36 +0300

    correction_model_event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  has_many: users, through: :attendances
-  has_many: attendances
+  has_many :users, through: :attendances
+  has_many :attendances
   belongs_to :organizer, class_name: "User"
   
   validates :start_date, presence: true
