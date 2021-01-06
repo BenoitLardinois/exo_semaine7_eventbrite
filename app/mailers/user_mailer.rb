98c4e404cb31178720b18965444a3e1ab84446da
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-06 13:34:06 +0300

    use_devise_gem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,8 +8,10 @@ class UserMailer < ApplicationMailer
     
     mail(to: @user.email, subject: 'Bienvenue chez nous!')
   end
+=begin
   def new_attendance_email(attendance)
     @attendance = attendance
     mail(to: @attendance.event.organizer.email, subject: "Nouveau participant à votre évènement!!!")
   end
+=end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,10 +1,12 @@
 class Attendance < ApplicationRecord
-  after_create :new_attendance_send
+  #after_create :new_attendance_send
   
   belongs_to :user
   belongs_to :event
-  
+
+=begin
   def new_attendance_send
    UserMailer.new_attendance_email(self).deliver_now
   end
+=end
 end
