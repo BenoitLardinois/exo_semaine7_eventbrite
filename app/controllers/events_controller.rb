0beece840c3a0e34a5dc9a45d906d5b35f962116
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-09 13:21:26 +0300

    creation_profil_page_user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,7 +22,7 @@ class EventsController < ApplicationController
     new_event.description = params[:event_description]
     new_event.price = params[:event_price]
     new_event.location = params[:event_location]
-    new_event.organizer_id = params[:event_organizer_id]
+    new_event.organizer_id = params[:event_organizer_id] 
     
     if new_event.save
       redirect_to events_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'events#index'
-  resources :events
-  
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'events#index'
+  resources :users
+  resources :events
 end
