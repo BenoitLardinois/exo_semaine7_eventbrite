3a7f81bb17deffde1df2705ff707da06ab9508ba
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-09 13:47:41 +0300

    creation_of_users_and_events_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+require 'table_print'
+
 User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
 
 10.times do |index|
   User.create(
   id: index + 1,
-  first_name: "Utilisateur n° #{index + 1}",
-  email: "user#{index + 1}@yopmail.com"
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  description: Faker::Lorem.paragraph(sentence_count: 2),
+  email: "user#{index + 1}@yopmail.com",
+  password: "azerty636"
   )
 end
-tp User.all, :email
+tp User.all
+
+
+10.times do |index|
+  Event.create(
+  id: index + 1,
+  start_date: Faker::Time.forward(days: 5,  period: :evening, format: :long),
+  duration: 25,
+  title: Faker::Games::Pokemon.name,
+  description: Faker::Lorem.paragraph(sentence_count: 2),
+  price: 20,
+  location: Faker::Address.city,
+  organizer_id: Faker::Number.between(from: 1, to: 10)
+  )
+end
+tp Event.all
