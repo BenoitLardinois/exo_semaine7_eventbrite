93395f70b85b6103d057b66d828d5f20a910ad8f
BenoitLardinois <benoitlardinois@hotmail.com>
2021-01-09 15:35:01 +0300

    new_migration_&_modification_view_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!#, only: [:show]
 
   def show
+#METHODE A CORRIGER !!
+=begin
+    @my_user = User.find(params[:id])
+    if current_user.is_admin || @my_user == current_user
+      redirect_to user_path
+    else
+      redirect_to new_user_session_path
+    end
+=end
     @my_user = User.find(params[:id])
   end
 end
